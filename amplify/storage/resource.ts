44523472ef90865fdcf452eb2970ce8899ad0805
Shweta kale <shwetakale144@gmail.com>
2024-05-26 14:38:29 +0530

    add storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,14 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive'
+  name: 'store-basic',
+  access: (allow) => ({
+    'audio/private/{entity_id}/*': [
+      allow.entity('identity').to(["read","write","delete"]),
+    ],
+    'text/*': [
+      allow.guest.to(['read', "write",]),
+      allow.authenticated.to(["read","write","delete"])
+    ]
+  })
 });
